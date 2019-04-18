c23a32296f725771b0284fc43c0090e0e5aa6eb6
Michael Potter <michaeljosephpotter101@gmail.com>
2019-04-18 13:42:08 -0700

    Removing sandbox content from boilerplate

--- project/App.js
+++ project/App.js
@@ -1,17 +1,10 @@
 // Import objects here
-import Calculator from './Calculator'
 
 // Global variables
-let calc;
 
 // The starting point for the application
 export default class App {
     constructor (config) {
         console.log ('App is starting! Config:', config)
-
-        calc = new Calculator (config);
-
-        console.log ('Running complex equation');
-        console.log (calc.planck (500, 5800))
     }
 }
